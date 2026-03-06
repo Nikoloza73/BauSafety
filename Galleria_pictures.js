4d5e82e7b2c38cf856176728c55d685b9b7fa0f4
Tato Nikoloz Shalikashvili <t.shalikashvili73@gmail.com>
2026-03-06 22:15:49 +0400

    change

--- Galleria_pictures.js
+++ Galleria_pictures.js
@@ -57,13 +57,13 @@ const photos = [
 ];
 
 const videos = [
-  'bausafety_videos/0-02-0b-0840ae4820453ece9b514b4ab5c8d750eaa1b55cfff08b71872689db1f4e6852_407032fdf2b00321.mp4',
-  'bausafety_videos/0-02-0b-cca0c6f6dc79dcf2ccfccf13b6625dfac916f6a55d73641d65b9404c64c41734_4d4ff21666e5c6b8.mp4',
-  'bausafety_videos/bausafety video.mp4',
-  'bausafety_videos/saswavlo centri.mp4',
-  'bausafety_videos/იმედი saswavlo centri.mp4',
-  'bausafety_videos/პრაქტიკული უნიქსში.mp4',
-  'bausafety_videos/სერტიფიკატების გადაცემა.mp4',
+  '0-02-0b-0840ae4820453ece9b514b4ab5c8d750eaa1b55cfff08b71872689db1f4e6852_407032fdf2b00321.mp4',
+  '0-02-0b-cca0c6f6dc79dcf2ccfccf13b6625dfac916f6a55d73641d65b9404c64c41734_4d4ff21666e5c6b8.mp4',
+  'bausafety video.mp4',
+  'saswavlo centri.mp4',
+  'იმედი saswavlo centri.mp4',
+  'პრაქტიკული უნიქსში.mp4',
+  'სერტიფიკატების გადაცემა.mp4',
 ];
 
 document.addEventListener('DOMContentLoaded', function() {
